31888037768c982e23097e5f249af8a2f5625197
Saurav Kumar <arvsaurav@gmail.com>
2021-01-11 02:16:40 +0530

    Initial commit

---
+++ app/src/main/java/com/gmail/sauravarv/covid19/MainActivity.java
@@ -0,0 +1,37 @@
+package com.gmail.sauravarv.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+{
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.myWebView);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.covid19india.org");
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(myWebView.canGoBack())
+        {
+            myWebView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+}
